d1840e690ca81f325fa964a11df59852c5e33232
Elmira Andreeva <elmira.andreeva@gmail.com>
2018-06-08 22:50:19 -0700

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/AndroidManifest.xml

---
+++ app/src/main/java/com/topic/elmira/androidtopics/edittext/EditTextActivity.java
@@ -0,0 +1,15 @@
+package com.topic.elmira.androidtopics.edittext;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.topic.elmira.androidtopics.R;
+
+public class EditTextActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_text);
+    }
+}
