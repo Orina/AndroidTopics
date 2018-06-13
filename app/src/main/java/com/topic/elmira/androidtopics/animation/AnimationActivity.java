c77b0de62a8f6530792da45f3717e0399d1bd900
Orina <elmira.andreeva@gmail.com>
2018-06-13 14:48:53 -0700

    added custom views

--- app/src/main/java/com/topic/elmira/androidtopics/animation/AnimationActivity.java
+++ app/src/main/java/com/topic/elmira/androidtopics/animation/AnimationActivity.java
@@ -2,12 +2,16 @@ package com.topic.elmira.androidtopics.animation;
 
 import android.animation.Animator;
 import android.animation.AnimatorInflater;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.BounceInterpolator;
 import android.view.animation.DecelerateInterpolator;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.topic.elmira.androidtopics.R;
@@ -15,6 +19,7 @@ import com.topic.elmira.androidtopics.R;
 public class AnimationActivity extends AppCompatActivity {
 
     private TextView textView;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +27,21 @@ public class AnimationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_animation);
 
         textView = findViewById(R.id.text_view);
+        button = findViewById(R.id.animateBtn);
+
+        final Animator btnAnimator = AnimatorInflater.loadAnimator(this, R.animator.slide_down_anim);
+        btnAnimator.setTarget(button);
+        btnAnimator.start();
+        btnAnimator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                ObjectAnimator anim = ObjectAnimator.ofFloat(textView, "ScaleY", 0, 1);
+                anim.setInterpolator(new BounceInterpolator());
+                anim.start();
+            }
+        });
+
     }
 
     public void startAnimation(View view){
